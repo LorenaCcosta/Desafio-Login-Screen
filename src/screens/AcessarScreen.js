d0eea6548d9a97ba710d5608e41448c9639e8bc5
LorenaCcosta <lorenaaraujo954@gmail.com>
2025-08-17 15:49:26 -0300

    Update AcessarScreen.js

--- src/screens/AcessarScreen.js
+++ src/screens/AcessarScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image, Dimensions } from "react-native";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -7,26 +7,35 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 const windowWidth = Dimensions.get('window').width;
 
 export default function AcessarScreen( {navigation} ){
+    const [mostrarSenha, setMostrarSenha] = useState(false);
+    const [checkbox, setCheckbox] = useState(false);
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+
     return(
         <View style={styles.container}>
             <TouchableOpacity onPress={() => navigation.goBack()} >
                 <Image style={styles.setaVoltar} source={require('../../assets/SetaVoltar.png')} />
             </TouchableOpacity>
+
             <Text style={styles.title}>Acesse</Text>
             <Text style={styles.subTitle}>Com E-mail e senha</Text>
 
             <Text style={styles.textEmail}>E-mail</Text>
-            <TextInput style={styles.inputEmail} placeholder="Digite seu E-mail" />
+            <TextInput style={styles.inputEmail}keyboardType="email-adress"  placeholder="Digite seu e-mail" onChangeText={setEmail}/>
 
             <Text style={styles.textSenha}>Senha</Text>
 
-            <TextInput style={styles.inputSenha} placeholder="Digite sua senha"/>
-            <TouchableOpacity style={styles.iconEye}>
-                <Icon name={'visibility'} size={24}/>
+            <TextInput style={styles.inputSenha} placeholder="Digite sua senha" secureTextEntry={!mostrarSenha} onChangeText={setSenha}/>
+
+            <TouchableOpacity style={styles.iconEye} onPress={() => setMostrarSenha(!mostrarSenha)}>
+                <Icon name={mostrarSenha ? 'visibility': 'visibility-off'} size={24}/>
 
             </TouchableOpacity>
             <View style={styles.container2}>
-                <TouchableOpacity style={styles.btn_checkbox}>
+                <TouchableOpacity style={styles.checkbox} onPress={() => setCheckbox(!checkbox)}>
+                <Icon name={checkbox ? 'check-box': 'check-box-outline-blank'} size={26} color={'#14C871'} />
 
             </TouchableOpacity>
                 <Text style={styles.textLembrarSenha}>Lembrar senha</Text>
@@ -44,11 +53,13 @@ export default function AcessarScreen( {navigation} ){
             </TouchableOpacity>
             </View>
 
-            <Text style={styles.textContinue}>Ou continue com</Text>
+                <Text style={styles.textContinue}>Ou continue com</Text>
 
-            <Image style={styles.google_logo} source={require('../../assets/Google.png')}/>
+            <View style={styles.container_logos}>
+                <Image style={styles.google_logo} source={require('../../assets/Google.png')}/>
 
-            <Image style={styles.facebook_logo} source={require('../../assets/Facebook.png')}/>
+                <Image style={styles.facebook_logo} source={require('../../assets/Facebook.png')}/>
+            </View>
         </View>
     );
 }
@@ -56,9 +67,8 @@ export default function AcessarScreen( {navigation} ){
 const styles =  StyleSheet.create({
     container: {
         flex: 1,
-        marginLeft: 22,
-        marginRight: 22,
-        marginTop: 14,
+        marginHorizontal: 20,
+        marginTop: 26,
     },
 
     container2: {
@@ -66,12 +76,8 @@ const styles =  StyleSheet.create({
         marginBottom: 24,
     },
 
-    btn_checkbox:{
-        height: 22,
-        width: 22,
-        borderColor: '#14C871',
-        borderWidth: 1, 
-        borderRadius: 4,
+    checkbox:{
+        top: '-10%'
     },
 
     container_Input: {
@@ -80,14 +86,14 @@ const styles =  StyleSheet.create({
 
     title: {
         color: "#31404D",
-        fontSize: 40,
+        fontSize: 42,
         fontWeight: 600,
     },
 
     setaVoltar: {
         height: 24,
         width: 24,
-        marginBottom: 12,
+        marginBottom: 14,
     },
 
     subTitle: {
@@ -101,7 +107,7 @@ const styles =  StyleSheet.create({
     },
 
     inputEmail: {
-        height: 68,
+        height: 62,
         backgroundColor: '#E3E7F3',
         borderRadius: 4,
         marginBottom: 16,
@@ -110,12 +116,13 @@ const styles =  StyleSheet.create({
     },
 
     inputSenha: {
-        height: 68,
+        height: 62,
         backgroundColor: '#E3E7F3',
         borderRadius: 4,
         marginBottom: 16,
         paddingLeft: 10,
         marginBottom: 26,
+        paddingHorizontal: 60,
     },
 
     textSenha: {
@@ -125,38 +132,70 @@ const styles =  StyleSheet.create({
 
     iconEye: {
         position: "absolute",
-        top: '36%',
+        top: '35%',
         right: 18,
     },
 
     textLembrarSenha: {
-        marginLeft: 8,
+        marginLeft: 5,
+        color: "#31404D",
+        fontWeight: 500,
     },
     textEsqueciSenha: {
-        marginLeft: '24%',
+        marginLeft: '23%',
+        color: "#31404D",
+        fontWeight: 500,
     },
 
     btn_green: {
         backgroundColor: '#14C871',
-        height: 68,
+        height: 62,
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 8,
+        width:"48%",
     },
 
     containerBtn: {
         flexDirection: 'row',
-        gap: 18,
+        justifyContent: 'center',
+        gap: 16,
+        marginBottom: 68,
 
     },
 
     btn_white: {
-        height: 68,
+        height: 62,
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 8,
         borderColor: '#14C871',
         borderWidth: 1,
+        width:"48%",
+        backgroundColor: '#ffff',
         
-    }
+    },
+
+    text_btn2: {
+        fontWeight: 500,
+        color: "#31404D",
+    },
+
+    text_btn1: {
+        color: "#ffff",
+        fontWeight: 500,
+    },
+
+    textContinue: {
+        textAlign: 'center',
+        fontWeight: 500,
+        color: '#31404D',
+        marginBottom: 28,
+    },
+
+    container_logos: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 34,
+    },
 })
